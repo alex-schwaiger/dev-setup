9f8d370597129f30a0623d27a6da148f686bfdf5
alex-schwaiger <schwaiger.alexander@gmx.at>
2025-11-29 12:08:59 +0100

    Add Laravel artisan scripts for project setup and cache management

---
+++ scripts/laravel/clearAllCaches.sh
@@ -0,0 +1,6 @@
+./vendor/bin/sail artisan optimize
+./vendor/bin/sail artisan config:clear
+./vendor/bin/sail artisan config:cache
+./vendor/bin/sail artisan cache:clear
+./vendor/bin/sail artisan route:cache
+./vendor/bin/sail artisan view:clear
---
+++ scripts/laravel/startLaravel.sh
@@ -0,0 +1,2 @@
+./vendor/bin/sail up -d
+npm run dev
